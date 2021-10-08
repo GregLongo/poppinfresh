012ff2c64569e4c03347c6feadf2328a29ea9611
Greg Longo <greglongo@Gregs-MacBook-Pro.local>
2021-10-08 12:30:37 -0700

    finished date timeline switch

--- pages/ThisStudent.js
+++ pages/ThisStudent.js
@@ -6,9 +6,9 @@ import  'firebase/compat/database'
 import styles from '../styles/Home.module.css'
 import { useRouter } from "next/router"
 import Link from 'next/link'
-
-
-
+import Highcharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official'
+import 'highcharts/modules/timeline'
 
 export default function ThisStudent({student}){
 
@@ -17,6 +17,7 @@ export default function ThisStudent({student}){
   ])
 
   const students = useSelector((state) => state.firebase.data.students)
+  const [selectDate, setDate] = useState(0)
 
   if (!isLoaded(students)) {
     return <div>Loading...</div>
@@ -25,13 +26,82 @@ export default function ThisStudent({student}){
   if (isEmpty(students)) {
     return <div>Students List Is Empty</div>
   }
+  const events = students[student].timestamps;
+  // [Date.parse(students["student1"].event.time), 0],;
+
+
+//Just Date
+const dates = [];
+
+Object.keys(events).map((key,id) =>{
+  var date = new Date(Date.parse(events[key].time));
+  var dateTrunc = Date.parse(date.getFullYear()+"/"+(date.getMonth()+1)+"/"+date.getDate());
+  if(!dates.includes(dateTrunc)){dates.push(dateTrunc)};
+})
+
+
+
+//all timestamps
+const timestamps = [];
+
+  Object.keys(events).map((key,id) =>{
+    var stamp = new Date(Date.parse(events[key].time));
+    if(stamp.getDate() == selectDate ) {timestamps.push([stamp,0])}
+  })
+
+  // Object.keys(events).map((key,id) =>{
+  //   var stamp = Date.parse(events[key].time);
+  //
+  //   timestamps.push([stamp,0]);
+  // })
+  // console.log(dates)
+
+  const options = {
+    title: {
+      text: ''
+    },
+    xAxis: {
+      type: 'datetime'
+    },
+    credits:{
+      enabled:false
+    },
+    chart: {
+        spacingBottom: 15,
+        spacingTop: 0,
+        spacingLeft: 10,
+        spacingRight: 10,
+        width: 700,
+        height: 300
+      },
+    series:[{
+      data:timestamps
+    }]
+  }
 
   return(
   <div>
     <div><img src={students[student].avatar} /></div>
     <div>{students[student].name}</div>
     <div>{students[student].age}</div>
-  </div>)
+    {dates.map((thisDate) =>{
+    var myDate = new Date(thisDate)
+
+    return <button onClick={() => setDate(myDate.getDate())}>{
+              myDate.getFullYear()+"/"+(myDate.getMonth()+1)+"/"+myDate.getDate()
+            }
+        </button>
+     })}
+    <HighchartsReact
+      highcharts={Highcharts}
+      options={options}
+    />
+
+
+
+  </div>
+
+)
 }
 
 ThisStudent.getInitialProps = ({query: { student } }) =>{
