bb5aad63cc2618e7612a56b2b005a197b533a4bb
Greg Longo <greglongo@Gregs-MacBook-Pro.local>
2021-10-11 15:42:23 -0700

    second timline

--- pages/ThisStudent.js
+++ pages/ThisStudent.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { useSelector } from 'react-redux'
 import { useFirebaseConnect, isLoaded, isEmpty } from 'react-redux-firebase'
@@ -9,30 +9,53 @@ import Link from 'next/link'
 import Highcharts from 'highcharts'
 import HighchartsReact from 'highcharts-react-official'
 import 'highcharts/modules/timeline'
+import BookTimeline from "../components/BookTimeline.js"
+
 
 export default function ThisStudent({student}){
 
   useFirebaseConnect([
     '/students',
+    '/popups'
   ])
 
   const students = useSelector((state) => state.firebase.data.students)
-  const [selectDate, setDate] = useState('12')
+  const popups = useSelector((state) => state.firebase.data.popups)
 
-  if (!isLoaded(students)) {
+  // const initState = useSelector((state) => state.firebase.data.students['student1'].timestamps['event1'].time)
+
+  const [selectDate, setDate] = useState()
+
+  // useEffect(() => {
+  //   setDate('12')
+  // });
+
+  const [thisPopup, selectPopup] = useState()
+
+
+
+    if (!isLoaded(students)) {
     return <div>Loading...</div>
   }
 
   if (isEmpty(students)) {
     return <div>Students List Is Empty</div>
   }
+  if (!isLoaded(popups)) {
+  return <div>Loading...</div>
+}
+
+if (isEmpty(popups)) {
+  return <div>Students List Is Empty</div>
+}
+
 
   const events = students[student].timestamps;
   // [Date.parse(students["student1"].event.time), 0],;
 
-  const initDate = new Date(events['event1'].time)
+  // const initDate = new Date(events['event1'].time)
 
-  console.log(Date.parse(initDate.getDate()))
+  // console.log(Date.parse(initDate.getDate()))
 //Just Date
 const dates = [];
 
@@ -50,15 +73,26 @@ const timestamps = [];
 
   Object.keys(events).map((key,id) =>{
     var stamp = new Date(Date.parse(events[key].time));
-    if(stamp.getDate() == selectDate ) {timestamps.push([stamp,0])}
+    var stampTrunc = stamp.getFullYear()+"/"+(stamp.getMonth()+1)+"/"+stamp.getDate();
+    // console.log("moo"+stampTrunc)
+    if(stampTrunc == selectDate ) {timestamps.push([stamp,0,'flobrb'])}
   })
 
-  // Object.keys(events).map((key,id) =>{
-  //   var stamp = Date.parse(events[key].time);
+  var lastevent ="LP2";
+  // console.log("sisisisi"+students[student].timestamps[lastevent].id);
+
+
+
+  const currentBook = popups[lastevent];
+
+// console.log("burrevimn"+currentBook.booktitle)
+  // const currentPages = booksList[currentBook].pages;
   //
-  //   timestamps.push([stamp,0]);
-  // })
-  // console.log(dates)
+  // console.log("Now Reading: "+popups[lastevent].booktitle)
+  // console.log("Chapter: "+popups[lastevent].title)
+  // // console.log("pages: "+currentPages)
+
+
 
   const options = {
     title: {
@@ -67,6 +101,7 @@ const timestamps = [];
     xAxis: {
       type: 'datetime'
     },
+
     credits:{
       enabled:false
     },
@@ -78,29 +113,77 @@ const timestamps = [];
         width: 700,
         height: 300
       },
+      tooltip:{
+          enabled: false
+      },
     series:[{
+      dataLabels: {
+        enabled: true
+      },
+      allowPointSelect: true,
+      point: {
+          events: {
+              click: function() {
+                  this.series.chart.update({
+                      tooltip: {
+                          enabled: false
+                      }
+                  });
+              },
+              mouseOut: function() {
+                  this.series.chart.update({
+                      tooltip: {
+                          enabled: false
+                      }
+
+                  })
+              },
+              select: function(events){
+                const poppers = students[student].timestamps['event'+((this.x)+1)].id
+                selectPopup(poppers);
+                console.log(popups[poppers].category);
+
+              }
+          }
+      },
       data:timestamps
     }]
   }
+
+
+  // useEffect(()=>{
+  //    var myDate = new Date(dates[1])
+  //    setDate(myDate.getFullYear()+"/"+(myDate.getMonth()+1)+"/"+myDate.getDate())
+  //  })
 
   return(
   <div>
     <div><img src={students[student].avatar} /></div>
     <div>{students[student].name}</div>
     <div>{students[student].age}</div>
-    {dates.map((thisDate) =>{
+    <div>Now Reading: {currentBook.booktitle}</div>
+    <div>{currentBook.title}</div>
+    <BookTimeline
+      lastevent= {lastevent}//pass me the id of the last item from "timestamps" via key
+    />
+    {dates.map((thisDate,index) =>{
     var myDate = new Date(thisDate)
 
-    return <button onClick={() => setDate(myDate.getDate())}>{
-              myDate.getFullYear()+"/"+(myDate.getMonth()+1)+"/"+myDate.getDate()
-            }
+    console.log(timestamps)
+    return <button onClick={() => {
+      setDate(myDate.getFullYear()+"/"+(myDate.getMonth()+1)+"/"+myDate.getDate())
+
+        const someevent = Object.keys(students[student].timestamps)[Object.keys(students[student].timestamps).length-1];
+         lastevent = students[student].timestamps[someevent].id;
+    }}>{
+        myDate.getFullYear()+"/"+(myDate.getMonth()+1)+"/"+myDate.getDate()
+      }
         </button>
      })}
     <HighchartsReact
       highcharts={Highcharts}
       options={options}
     />
-
 
 
   </div>
